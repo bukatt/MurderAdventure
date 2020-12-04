b4046f52fb5f1a6d0959e4c8623519ce8b495baa
bukatt <pstenger13@gmail.com>
2020-12-03 21:45:05 -0500

    fonts

---
+++ Assets/Scripts/OnHoverChangeColor_Text.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.EventSystems;
+
+public class OnHoverChangeColor_Text : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Color defaultColor;
+    public Color hoverColor;
+
+    public TMP_Text text;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        text.color = hoverColor;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        text.color = defaultColor;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.color = defaultColor;
+    }
+
+
+}
